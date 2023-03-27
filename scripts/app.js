a0a443e97c4e33f65a0ed228b7bd0db9beb989b2
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-03-28 04:21:00 +0430

    playe musics on musicPlayer handled

--- scripts/app.js
+++ scripts/app.js
@@ -43,11 +43,31 @@ let introSongs = [
   },
 ];
 
-let introSongsWrapper = document.querySelector(".intro-songs-wrapper");
-let mainAudio = document.querySelector(".main-audio");
+const introSongsWrapper = document.querySelector(".intro-songs-wrapper");
+const mainAudio = document.querySelector(".main-audio");
+// music Player
+const playerInfoImg = document.querySelector(".player-info__img img");
+const playerInfoName = document.querySelector(".player-info__name");
+const playerInfoArtist = document.querySelector(".player-info__artist");
+const playerPlayBtn = document.querySelector(".fa-play-circle");
+const playerControlDuration = document.querySelector(
+  ".player-control__duration"
+);
+const playerControlCurrent = document.querySelector(".player-control__current");
+const playerControlVolume = document.querySelector(
+  ".player-control__volume input"
+);
+let playerPrograssBar = document.querySelector(".player-prograss__fit");
+let isPlaying = false;
+let i = 0;
 
 introSongs.forEach(function (music) {
-  createIntroMuics(music);
+  i++;
+  if (i < 5) {
+    createIntroMuics(music);
+  } else {
+    console.log(i);
+  }
 });
 
 function createIntroMuics(music) {
@@ -59,12 +79,52 @@ function createIntroMuics(music) {
       music.musicName +
       '</div>    <div class="intro-song__artist">' +
       music.artist +
-      '</div>  </div>  <div class="intro-song-control">    <div class="intro-control__play"><i class="fa fa-play" onclick="loadMuisc(' +
+      '</div>  </div>  <div class="intro-song-control">    <div class="intro-control__play"><i class="fa fa-play" onclick="loadMusic(' +
       music.id +
       ')"></i></div>     <div class="intro-control__like"><i class="fa fa-heart"></i></div>    <div class="intro-control__download"><i class="fa fa-download"></i></div>    <div class="intro-control__add"><i class="fa fa-plus"></i></div>  </div>  </div>'
   );
 }
 
-function loadMuisc(muiscID) {
-  
+function loadMusic(muiscID) {
+  muiscID = muiscID - 1;
+  playerInfoName.textContent = introSongs[muiscID].musicName;
+  playerInfoArtist.textContent = introSongs[muiscID].artist;
+  playerInfoImg.setAttribute("src", introSongs[muiscID].cover);
+  mainAudio.setAttribute("src", introSongs[muiscID].path);
+  playMusic();
 }
+
+function playMusic() {
+  mainAudio.play();
+  isPlaying = true;
+  playerPlayBtn.className = "fa fa-pause-circle";
+}
+
+function pauseMusic() {
+  mainAudio.pause();
+  isPlaying = false;
+  playerPlayBtn.className = "fa fa-play-circle";
+}
+
+let musicDuration;
+let musicCurrent;
+let prograssBarPercent;
+function updatePorgrassBar(e) {
+  musicDuration = e.srcElement.duration;
+  musicCurrent = e.srcElement.currentTime;
+  // prograss percent
+  prograssBarPercent = (musicCurrent / musicDuration) * 100;
+  playerPrograssBar.style.width = prograssBarPercent + "%";
+  // duration and current time set
+  // musicDuration = Math.floor(musicDuration / 60);
+  // musicCurrent = Math.floor(musicCurrent / 60);
+}
+
+playerPlayBtn.addEventListener("click", function () {
+  if (isPlaying) {
+    pauseMusic();
+  } else {
+    playMusic();
+  }
+});
+mainAudio.addEventListener("timeupdate", updatePorgrassBar);
